0b66d2a6870d3c10075bb63adb1a341d8dc19cfd
Jonathan <jonnygim.dev@gmail.com>
2022-08-08 20:44:40 +0900

    Create project

---
+++ src/main/java/com/jgim/flowerparty/FlowerpartyApplication.java
@@ -0,0 +1,13 @@
+package com.jgim.flowerparty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowerpartyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowerpartyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jgim/flowerparty/FlowerpartyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jgim.flowerparty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowerpartyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
